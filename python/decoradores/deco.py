45f60edd435ed015efdfa10aa04650aa1e3fad52
jk10s <juanka_267@hotmail.com>
2022-06-21 13:46:20 -0500

    decoradores

---
+++ python/decoradores/deco.py
@@ -0,0 +1,13 @@
+def decorador(deco1):
+    def deco2():
+        print("esya l")
+        deco1()
+    return deco2
+
+@decorador
+def suma():
+    
+    print("este es una linera uno")
+
+suma()
+""" version air animal anwer acholo art arte baby woik dass  """
